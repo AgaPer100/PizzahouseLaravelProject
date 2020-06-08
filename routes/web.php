b3a0889fb69481d57d5ea5de65d8adb966fd9652
Agnieszka Perkowska <agaper100@gmail.com>
2020-06-09 00:30:25 +0200

    created controller

--- routes/web.php
+++ routes/web.php
@@ -17,21 +17,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/pizzas', function () {
-    //get data from db
-    
-    $pizzas = [
-        ['type' => 'hawaiian', 'base' => 'cheesy crust'],
-        ['type' => 'volcano', 'base' => 'garlic crust'],
-        ['type' => 'veg supreme', 'base' => 'thin & crispy']
-    ];
+Route::get('/pizzas', 'PizzaController@index');
 
-    return view('pizzas', [
-        'pizzas'=> $pizzas,
-        ]);
-});
-
-Route::get('/pizzas/{id}', function ($id) {
-
-    return view('details', ['id' => $id]);
-});
+Route::get('/pizzas/{id}','PizzaController@show');
